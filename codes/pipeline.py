3f5a9613bafada9550c369be86bb0ed2bd8d27ee
Cameron Liang <cameron.liang@gmail.com>
2020-06-15 14:32:00 -0500

    updated pipeline py file name

--- codes/pipeline.py
+++ codes/pipeline.py
@@ -60,9 +60,6 @@ def sql_select_data(prod_id):
 
 if __name__ == '__main__': 
 
-    
-    
-
     # input -- current filename 
     # this includes all the features, and demand. 
     # then fit the model 
