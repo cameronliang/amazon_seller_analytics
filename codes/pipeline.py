1aa1329e8619dc1ff08ff24d35b31677692b1d86
Cameron Liang <cameron.liang@gmail.com>
2020-06-15 14:30:22 -0500

    added sql database to pipeline

--- codes/pipeline.py
+++ codes/pipeline.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+
 def AddData(datafile_path):
     # copy old file and save it for backup
     current_sales_fname = datafile_path + '/sales.csv'
@@ -23,11 +27,41 @@ def AddData(datafile_path):
     return df
 
 
+def read_credential():
+    fname = './sql_credential.dat'
+    credential = []
+    with open(fname) as f:
+        for line in f:
+            line = line.rstrip().split(' ')
+            credential.append(line[1])
+    return credential
 
+
+def sql_select_data(prod_id):
+    dbname,username,pswd = read_credential()
+
+    engine = create_engine('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))
+    #print('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))
+    #print(engine.url)
+
+    # connect to database. 
+    con = None
+    con = psycopg2.connect(database = dbname, user = username, host='localhost', password=pswd)
+
+    # query:
+    sql_query = """
+    SELECT * FROM sales_data_table WHERE product_id='%s';
+    """ % prod_id
+
+
+    sales_data = pd.read_sql_query(sql_query,con)
+    return sales_data
 
 
 if __name__ == '__main__': 
 
+    
+    
 
     # input -- current filename 
     # this includes all the features, and demand. 
@@ -36,4 +70,7 @@ if __name__ == '__main__':
     # compute 
     #current_sales_fname = '../../cleaned_data/simulations/sales.csv'
     datafile_path = '../../cleaned_data/simulations/'
-    df = AddData(datafile_path)
+    #df = AddData(datafile_path)
+
+    
+    sql_select_data('prod1')
